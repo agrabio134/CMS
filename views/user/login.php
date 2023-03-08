ebb4dd1c80bf43e8fb96b77dff69c142d4083162
John Harvey Agrabio <agrabioharvey@gmail.com>
2023-03-08 16:27:37 +0800

    add html

--- views/user/login.php
+++ views/user/login.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
 <?php
   // Check if the user is already logged in
   if (isset($_SESSION['user_id'])) {
@@ -20,3 +30,6 @@
   <input type="password" name="password" id="password"><br><br>
   <input type="submit" value="Log in">
 </form>
+  
+</body>
+</html>
